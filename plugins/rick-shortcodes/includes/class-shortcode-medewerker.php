59789c28421bc6ab5f43dab20675bfb807bf9185
Rick van Houten <00318290@rijnijssel.nl>
2026-03-11 15:39:36 +0100

    updated .gitignore - alle mappen structuur - shortcode medewerker

---
+++ plugins/rick-shortcodes/includes/class-shortcode-medewerker.php
@@ -0,0 +1,44 @@
+<?php 
+if ( !defined('ABSPATH')) exit; /* voorkomt toegang vanaf browser */
+
+class Shortcode_Medewerker {
+
+    public function __construct() {
+        add_shortcode( 'medewerker_kaart', [ $this, 'render' ]);
+    }
+
+    public function render( $atts ) {
+        /* standaard waarden voor id = 0 */
+        $atts = shortcode_atts( [
+            'id' => 0,
+        ], $atts, 'medewerker_kaart' );
+
+        $post_id = intval( $atts['id'] );
+ 
+        /* checken of de post bestaat anders niks laten zien nu tijdelijk om gezet naar error: medewerker niet gevonden voor test */
+        if ( ! $post_id || ! get_post( $post_id ) ) return 'error: medewerker niet gevonden';
+        
+        $functietitel = get_field( 'functietitel', $post_id );
+        $afdeling     = get_field( 'afdeling', $post_id );
+        $telefoon     = get_field( 'telefoonnummer', $post_id );
+        $foto         = get_field( 'profielfoto', $post_id); 
+
+        $output = '<div class="medewerker-kaart border p-4 rounded">';
+
+        if ( $foto ) {
+            /* URL ophalen uit de array uit het ACF field */
+            $foto_url = $foto['sizes']['medium'] ?? $foto['url'] ?? '';
+            if ( $foto_url ) {
+                $output .= '<div class="medewerker-foto mb-2" style="background-image: url(' . esc_url( $foto_url ) . '); background-size: cover; background-position: center; width: 10rem; height: 10rem;"></div>';
+            }
+        }
+
+        if ( $functietitel ) $output .= '<h3>' . esc_html( $functietitel ) . '</h3>';
+        if ( $afdeling ) $output .= '<p>' . esc_html( $afdeling ) . '</p>';
+        if ( $telefoon ) $output .= '<p>' . esc_html( $telefoon ) . '</p>';
+
+        $output .= '</div>';
+
+        return $output;
+    }
+}
---
+++ plugins/rick-shortcodes/rick-shortcodes.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name: Rick Shortcodes
+ * Description: Voorbeeld plugin met 6 shortcodes.
+ * Version:     1.0.0
+ * Author:      Rick van Houten
+ */ 
+ 
+
+// Verplicht in elke plugin: voorkomt dat dit bestand direct via de browser wordt aangeroepen.
+// ABSPATH is een constante die WordPress definieert. Als die er niet is, draait dit buiten WP.
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Hoofd plugin class
+ */
+class Rick_Shortcodes_Plugin {
+
+    public function __construct() {
+        // benodigde bestanden laden
+        $this->includes();
+
+        // classes starten
+        $this->init_classes();
+    }
+
+    /**
+     * Inladen van de bestanden in de includes map
+     */
+    private function includes() {
+
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-shortcode-medewerker.php';
+
+    
+    }
+
+    /**
+     * activeren van de classes 
+     */ 
+    private function init_classes() {
+
+        new Shortcode_Medewerker();
+
+    }
+}
+
+ /**
+  * Plugin starten
+  */
+  function rick_shortcodes_init() {
+    new Rick_Shortcodes_Plugin();
+  }    
+
+  add_action( 'plugins_loaded', 'rick_shortcodes_init' );
+
+  /**
+   * Activatie hook
+   */
+  function rick_shortcodes_activate() {
+    /* Dit zorgt dat WordPress de URL structuur opnieuw opbouwt wanneer de plugin geactiveerd wordt */
+    flush_rewrite_rules();
+  }
+
+  register_activation_hook( __FILE__, 'rick_shortcodes_activate' );
